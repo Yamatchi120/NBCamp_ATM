e3af4da6bfb92f2554996bc4ad718a7fb0e71f46
Yamatchi <ussousso12@gmail.com>
2025-06-04 15:15:08 +0900

    feat : 변수 추가
    userName
    cash
    bank

---
+++ Assets/Scripts/Data/UserData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class UserData : MonoBehaviour
+{
+    private string userName;
+    public string UserName { get; private set; }
+    private int cash;
+    public int Cash { get; private set; }
+    private int bank;
+    public int Bank { get; private set; }
+}
