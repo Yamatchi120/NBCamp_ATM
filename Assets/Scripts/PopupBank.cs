6908f5bf0e36cac3fb2b784cbf0829d59f48f737
Yamatchi <ussousso12@gmail.com>
2025-06-05 10:34:02 +0900

    feat : 스크립트 수정
    OnDepositCustomBtn()
    OnWIthhdrawCustomBtn()
    InputAmount() 추가

--- Assets/Scripts/PopupBank.cs
+++ Assets/Scripts/PopupBank.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,10 +8,14 @@ public class PopupBank : MonoBehaviour
     [SerializeField] private GameObject atm;
     [SerializeField] private GameObject depositBtn;
     [SerializeField] private GameObject withdrawBtn;
+    [SerializeField] private TMP_InputField inputField;
 
     private UserData userData;
+    int inputAmount;
+    string userCustom;
     private void Start()
     {
+        userCustom = inputField.text;
         userData = GameManager.Instance.userData;
     }
     public void OnDepositBtn()
@@ -35,16 +40,29 @@ public class PopupBank : MonoBehaviour
     }
     public void OnClickDeposit(int amount)
     {
-        Debug.Log("OnClickDeposit");
-        Debug.Log($"amount = {amount}");
-
-        Debug.Log($"Cash = {userData.Cash}");
-        Debug.Log($"Balance = {userData.Balance}");
-
         userData.DepositCash(amount);
     }
     public void OnClickWithdraw(int amount)
     {
         userData.WithdrawBalance(amount);
+    }
+    public void OnDepositCustomBtn()
+    {
+        InputAmount();
+        userData.DepositCash(inputAmount);
+    }
+    public void OnWithdrawCustomBtn()
+    {
+        InputAmount();
+        userData.WithdrawBalance(inputAmount);
+    }
+    private void InputAmount()
+    {
+
+        if(int.TryParse(userCustom, out inputAmount))
+        {
+            Debug.Log($"입력된 숫자 -> {inputAmount}");
+        }
+        else { Debug.LogWarning("숫자가 아닙니다."); }
     }
 }
